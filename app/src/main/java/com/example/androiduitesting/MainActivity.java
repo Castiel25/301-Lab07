cb78a03f547ff4fcaf56fbeccdd787c86e4254f6
Yuze Liaocheng <liaochen@ualberta.ca>
2026-02-27 15:06:49 -0700

    the ShowActivity and return logic implemented

--- app/src/main/java/com/example/androiduitesting/MainActivity.java
+++ app/src/main/java/com/example/androiduitesting/MainActivity.java
@@ -41,6 +41,19 @@ public class MainActivity extends AppCompatActivity {
 
         cityList.setAdapter(cityAdapter);
 
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            String clickedCity = cityAdapter.getItem(position);
+
+            android.content.Intent intent = new android.content.Intent(MainActivity.this, ShowActivity.class);
+            intent.putExtra(ShowActivity.EXTRA_CITY_NAME, clickedCity);
+            startActivity(intent);
+        });
+
+
+
+
+
+
         final Button addButton = findViewById(R.id.button_add);
         addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
---
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -0,0 +1,32 @@
+package com.example.androiduitesting;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowActivity extends AppCompatActivity {
+    public static final String EXTRA_CITY_NAME = "extra_city_name";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+        TextView cityText = findViewById(R.id.text_city_name);
+
+        Button backButton = findViewById(R.id.button_back);
+
+        /*Intent intent = getIntent();*/
+        String cityName = getIntent().getStringExtra(EXTRA_CITY_NAME);
+
+        if (cityName == null) {cityName = "";}
+        cityText.setText(cityName);
+
+        backButton.setOnClickListener(v -> {
+            finish();
+        });
+    }
+
+}
